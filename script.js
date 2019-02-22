2c27712938e59f9822d588bdc94c63a5877d86a3
aubrin cole <dcolwell@juno.com>
2019-02-22 15:40:36 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// "el" stands for the "element" that was just clicked
+function addClass(el){
+	el.classList.add("opaque");
+
+}
+
+// "el" stands for the "element" that was just clicked
+function pass(el){	
+	var input = document.getElementById('pass').value
+}
+
+// password: 
